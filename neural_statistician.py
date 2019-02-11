18e799615758ae1a7bb3c089b6032e4308ace614
Ross Clarke <rmc78@cam.ac.uk>
2019-02-11 15:53:42 +0000

    Wrote LatentDecoder; added helpful comments with distributions

--- neural_statistician.py
+++ neural_statistician.py
@@ -1,10 +1,35 @@
 import torch as to
 import torch.nn.functional as F
 
+### p(z_i | z_(i+1), c) parameterised by theta
 class LatentDecoder(to.nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+
+        # Input is dim(c); there's no z_(i+1) in this application
+        self.dense1 = to.nn.Linear(3, 128)
+        self.dense2 = to.nn.Linear(128, 128)
+        self.dense3 = to.nn.Linear(128, 128)
+
+        # Output is dim(z) for the mean and dim(z) for the variance
+        self.final = to.nn.Linear(128, 2 * 32)
+
+    def forward(self, c):
+        w = self.dense1(c)
+        w = F.relu(w)
+
+        w = self.dense2(w)
+        w = F.relu(w)
+        
+        w = self.dense3(w)
+        w = F.relu(w)
+
+        w = self.final(w)
+        # We've now computed mu_z and log sigma_c
+        return w
 
 
+### p(x | z_(1:L), c) parameterised by theta
 class ObservationDecoder(to.nn.Module):
     def __init__(self):
         super().__init__()
@@ -14,7 +39,7 @@ class ObservationDecoder(to.nn.Module):
         self.dense2 = to.nn.Linear(128, 128)
         self.dense3 = to.nn.Linear(128, 128)
 
-        # Output is dim(x) for the man and dim(x) for the variance
+        # Output is dim(x) for the mean and dim(x) for the variance
         self.final = to.nn.Linear(128, 2 * 1)
 
     def forward(self, z, c):
@@ -35,10 +60,12 @@ class ObservationDecoder(to.nn.Module):
         return w
 
 
+### q(c | D) parameterised by phi
 class StatisticNetwork(to.nn.Module):
     pass
 
 
+### q(z_i | z_(i+1), c, x) parameterised by phi
 class InferenceNetwork(to.nn.Module):
     def __init__(self):
         super().__init__()
