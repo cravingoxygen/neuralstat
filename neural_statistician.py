76de1b370a8c3c78b9f7c868cef6652723c4307b
Ross Clarke <rmc78@cam.ac.uk>
2019-02-13 10:50:19 +0000

    Created skeleton tests file

--- neural_statistician.py
+++ neural_statistician.py
@@ -1,129 +1,47 @@
 import torch as to
 import torch.nn.functional as F
 
-### p(z_i | z_(i+1), c) parameterised by theta
-class LatentDecoder(to.nn.Module):
-    def __init__(self):
+import one_dimensional_test as ex
+
+
+class NeuralStatistician(object):
+    """Tying-together class to hold references for a particular experiment"""
+
+    def __init__(self, num_stochastic_layers):
         super().__init__()
 
-        # Input is dim(c); there's no z_(i+1) in this application
-        self.dense1 = to.nn.Linear(3, 128)
-        self.dense2 = to.nn.Linear(128, 128)
-        self.dense3 = to.nn.Linear(128, 128)
+        self.latent_decoders = [ex.LatentDecoder() for _ in num_stochastic_layers]
+        self.observation_decoder = ex.ObservationDecoder()
+        self.statistic_network = ex.StatisticNetwork()
+        self.inference_networks = [ex.InferenceNetwork() for _ in num_stochastic_layers]
 
-        # Output is dim(z) for the mean and dim(z) for the variance
-        self.final = to.nn.Linear(128, 2 * 32)
+        self.context_prior = to.distributions.multivariate_normal.MultivariateNormal(
+            loc=to.zeros(ex.context_dimension), covariance_matrix=to.eye(ex.context_dimension))
 
-    def forward(self, c):
-        w = self.dense1(c)
-        w = F.relu(w)
+    def compute_context_divergence(self, num_iterations=100):
+        pass
 
-        w = self.dense2(w)
-        w = F.relu(w)
+    def compute_loss(self, distribution_parameters):
+        """Compute the full model loss function"""
+        # Context divergence
+        # Expectation, under q(c | D; phi), of log (q(c|D;phi) / p(c))
         
-        w = self.dense3(w)
-        w = F.relu(w)
 
-        w = self.final(w)
-        # We've now computed mu_z and log sigma_c
-        return w
+    def predict(self):
+        pass
 
+    def train(self, data, num_iterations):
+        """Train the Neural Statistician"""
 
-### p(x | z_(1:L), c) parameterised by theta
-class ObservationDecoder(to.nn.Module):
-    def __init__(self):
-        super().__init__()
+        network_parameters = None
+        optimiser = to.optim.Adam(network_parameters,
+                                  lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0)
 
-        # Input is dim(z) + dim(z)
-        self.dense1 = to.nn.Linear(32 + 3, 128)
-        self.dense2 = to.nn.Linear(128, 128)
-        self.dense3 = to.nn.Linear(128, 128)
+        for iteration in num_iterations:
+            distribution_parameters = self.predict(data)
+            loss = self.compute_loss(distribution_parameters)
 
-        # Output is dim(x) for the mean and dim(x) for the variance
-        self.final = to.nn.Linear(128, 2 * 1)
-
-    def forward(self, z, c):
-        """Computes x from a concatenation (w) of latent variables z and context c."""
-        w = to.stack((z, c), dim=1)
-
-        w = self.dense1(w)
-        w = F.relu(w)
-
-        w = self.dense2(w)
-        w = F.relu(w)
-        
-        w = self.dense3(w)
-        w = F.relu(w)
-
-        w = self.final(w)
-        # We've now computed mu_x and log sigma_x
-        return w
-
-
-### q(c | D) parameterised by phi
-class StatisticNetwork(to.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Input is whole dataset; a batch of dim(x)
-        self.embed_dense1 = to.nn.Linear(1, 128)
-        self.embed_dense2 = to.nn.Linear(128, 128)
-        self.embed_dense3 = to.nn.Linear(128, 128)
-
-        self.post_pool_dense1 = to.nn.Linear(128, 128)
-        self.post_pool_dense2 = to.nn.Linear(128, 128)
-        # Output is Gaussian parameters for c
-        self.post_pool_dense3 = to.nn.Linear(128, 2 * 3)
-
-    def forward(self, data):
-        data = self.embed_dense1(data)
-        data = F.relu(data)
-        data = self.embed_dense2(data)
-        data = F.relu(data)
-        data = self.embed_dense3(data)
-        data = F.relu(data)
-
-        data = data.mean(dim=0)
-
-        data = self.post_pool_dense1(data)
-        data = F.relu(data)
-        data = self.post_pool_dense2(data)
-        data = F.relu(data)
-        data = self.post_pool_dense3(data)
-
-        return data
-
-
-### q(z_i | z_(i+1), c, x) parameterised by phi
-class InferenceNetwork(to.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        # Input is dim(c) + dim(x); there is no z_previous in this problem
-        self.dense1 = to.nn.Linear(3 + 1, 128)
-        self.dense2 = to.nn.Linear(128, 128)
-        self.dense3 = to.nn.Linear(128, 128)
-
-        # z is 32-D, outputting a mean and diagonal variance for the Gaussian
-        # distribution defining z
-        self.final = to.nn.Linear(128, 2 * 32)
-
-    def forward(self, x, c):
-        """Computes x from a concatenation (w) of latent variables z and context c."""
-        w = to.stack((c, x), dim=1)
-
-        w = self.dense1(w)
-        w = F.relu(w)
-
-        w = self.dense2(w)
-        w = F.relu(w)
-        
-        w = self.dense3(w)
-        w = F.relu(w)
-
-        w = self.final(w)
-        # We've now computed mu_x and log sigma_x
-        return w
-
-
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
 
