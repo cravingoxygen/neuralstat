Fixed non-square rooted variance in normal data generation

one_dimensional_test.py
@@ -137,7 +137,7 @@ class OneDimensionalDataset(to.utils.data.TensorDataset):
         variances = np.random.uniform(0.5, 2, 10000)
 
         data[0:2500] = np.random.exponential(np.sqrt(variances[0:2500]), (200, 2500)).T
-        data[2500:5000] = np.random.normal(means[2500:5000], variances[2500:5000], (200, 2500)).T
+        data[2500:5000] = np.random.normal(means[2500:5000], np.sqrt(variances[2500:5000]), (200, 2500)).T
         data[5000:7500] = np.random.uniform(means[5000:7500] - np.sqrt(3*variances[5000:7500]),
                                             means[5000:7500] + np.sqrt(3*variances[5000:7500]), (200, 2500)).T
         data[7500:10000] = np.random.laplace(means[7500:10000], np.sqrt(variances[7500:10000]/2), (200, 2500)).T